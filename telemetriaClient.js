5447c2fe4a95f83cc42344fc6559da7788e739d3
enrikelr <enrikelr@hotmail.com>
2016-12-08 17:48:44 +0100

    guardar archivo telemetria local
    guarda en un json  la telemetria de la session

--- telemetriaClient.js
+++ telemetriaClient.js
@@ -1,41 +1,54 @@
+var irsdk = require('node-irsdk')
 var file = require('file-system');
 var fs = require('fs');
+var moment = require('moment')
+var datosTelemetria={}
+
 // update telemetry as fast as possible
 var io =require('socket.io-client');
-irsdk.init({telemetryUpdateInterval: 0})
+irsdk.init({telemetryUpdateInterval: 3000})
 var iracing = irsdk.getInstance()
 var socket =io('http://vps259018.ovh.net:2389')
+
 function saveSample (type, time, data) {
   var fileName = './telemetria/' + time + '-' + type + '.json'
   file.writeFile(fileName, JSON.stringify(data), function (err) {
     if (err) throw err
   })
 }
+
 iracing.on('Connected', function () {
   console.log('connected')
-  iracing.on('SessionInfo', function (data) {
-  console.log('Telemetry:',data.data.CarSetup)
-  socket.emit("CarSetup",data.data.CarSetup)
+  iracing.once('TelemetryDescription', function (data) {
+	//console.log('TelemetryDesc:', data)
+	 var dateStr = moment().format().replace(/:/g, '')
+	var fileName = './telemetria/' + dateStr + '-telemetry-desc.json'
+	fs.writeFile(fileName, JSON.stringify(data, null, 2), function (err) {
+		if (err) throw err
+	})
+	for(var i in data){
+		datosTelemetria[i]=[]
+	}
   })
+	iracing.on('SessionInfo', function (data) {
+		//console.log('Telemetry:',data.data.CarSetup)
+		socket.emit("CarSetup",data.data.CarSetup)
+	})
   
-  iracing.on('Telemetry', function (data) {
-  console.log('Telemetry:', data.values.Speed)
-  socket.emit("speed",data.values.Speed)
-})
-iracing.on('TelemetryDescription', function (data) {
-  console.log('got TelemetryDescription')
-
-  saveSample('TelemetryDescription', Date.now(), data)
+	iracing.on('Telemetry', function (data) {
+	console.log('Telemetry:', data.values.Speed)
+	socket.emit("speed",data.values.Speed)
+	for(var i in data.values){
+		datosTelemetria[i].push(data.values[i])
+		}
 })
 
-iracing.on('Telemetry', function (data) {
-  console.log('got Telemetry')
-  saveSample('Telemetry', Date.now(), data)
 })
-
-iracing.on('SessionInfo', function (data) {
-  console.log('got SessionInfo')
-
-  saveSample('SessionInfo', Date.now(), data)
-})
+iracing.on('Disconnected', function () {
+  console.log('disconnected')
+  var dateStr = moment().format().replace(/:/g, '')
+	var fileName = './telemetria/' + dateStr + '-telemetry.json'
+	fs.writeFile(fileName, JSON.stringify(datosTelemetria), function (err) {
+		if (err) throw err
+	})
 })
