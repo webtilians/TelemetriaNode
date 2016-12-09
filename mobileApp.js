6190defb19affb237875459e4850443918d1f312
Jairo <kidandcat@gmail.com>
2016-12-09 09:41:42 +0100

    v1

--- mobileApp.js
+++ mobileApp.js
@@ -1,48 +1,92 @@
 var RTCPeerConnection = /*window.RTCPeerConnection ||*/ window.webkitRTCPeerConnection || window.mozRTCPeerConnection;
 
-if (RTCPeerConnection) (function () {
-    var rtc = new RTCPeerConnection({iceServers:[]});
-    if (1 || window.mozRTCPeerConnection) {      // FF [and now Chrome!] needs a channel/stream to proceed
-        rtc.createDataChannel('', {reliable:false});
+if (RTCPeerConnection)(function() {
+    var rtc = new RTCPeerConnection({
+        iceServers: []
+    });
+    if (1 || window.mozRTCPeerConnection) { // FF [and now Chrome!] needs a channel/stream to proceed
+        rtc.createDataChannel('', {
+            reliable: false
+        });
     };
-    
-    rtc.onicecandidate = function (evt) {
+
+    rtc.onicecandidate = function(evt) {
         // convert the candidate to SDP so we can run it through our general parser
         // see https://twitter.com/lancestout/status/525796175425720320 for details
-        if (evt.candidate) grepSDP("a="+evt.candidate.candidate);
+        if (evt.candidate) grepSDP("a=" + evt.candidate.candidate);
     };
-    rtc.createOffer(function (offerDesc) {
+    rtc.createOffer(function(offerDesc) {
         grepSDP(offerDesc.sdp);
         rtc.setLocalDescription(offerDesc);
-    }, function (e) { console.warn("offer failed", e); });
-    
-    
+    }, function(e) {
+        console.warn("offer failed", e);
+    });
+
+
     var addrs = Object.create(null);
     addrs["0.0.0.0"] = false;
+
     function updateDisplay(newAddr) {
         if (newAddr in addrs) return;
         else addrs[newAddr] = true;
-        var displayAddrs = Object.keys(addrs).filter(function (k) { return addrs[k]; });
-        
-        LOCAL IP
+        var displayAddrs = Object.keys(addrs).filter(function(k) {
+            return addrs[k];
+        });
+
+        displayAddrs.forEach(function(ip) {
+            findServerInLan(ip)
+        })
     }
-    
+
     function grepSDP(sdp) {
         var hosts = [];
-        sdp.split('\r\n').forEach(function (line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
-            if (~line.indexOf("a=candidate")) {     // http://tools.ietf.org/html/rfc4566#section-5.13
-                var parts = line.split(' '),        // http://tools.ietf.org/html/rfc5245#section-15.1
+        sdp.split('\r\n').forEach(function(line) { // c.f. http://tools.ietf.org/html/rfc4566#page-39
+            if (~line.indexOf("a=candidate")) { // http://tools.ietf.org/html/rfc4566#section-5.13
+                var parts = line.split(' '), // http://tools.ietf.org/html/rfc5245#section-15.1
                     addr = parts[4],
                     type = parts[7];
                 if (type === 'host') updateDisplay(addr);
-            } else if (~line.indexOf("c=")) {       // http://tools.ietf.org/html/rfc4566#section-5.7
+            } else if (~line.indexOf("c=")) { // http://tools.ietf.org/html/rfc4566#section-5.7
                 var parts = line.split(' '),
                     addr = parts[2];
                 updateDisplay(addr);
             }
         });
     }
-})(); else {
-    document.getElementById('list').innerHTML = "<code>ifconfig | grep inet | grep -v inet6 | cut -d\" \" -f2 | tail -n1</code>";
-    document.getElementById('list').nextSibling.textContent = "In Chrome and Firefox your IP should display automatically, by the power of WebRTCskull.";
+})();
+else {
+    console.log('WebRTC not available, IP detection disabled, falling back to standard networks (192.168.0.0/24, 192.168.1.0/24)');
+    findServerInLan('192.168.1.1');
+    findServerInLan('192.168.0.1');
+}
+
+
+function findServerInLan(ip) {
+    if (ip.split('.').length != 4) {
+        console.log('IPv6 not supported.');
+        return false;
+    }
+    var ip = ip || '192.168.0.1';
+    console.log('Searching network ' + ip + '/24 for a valid server.');
+    var aip = ip.split('.');
+    aip.pop();
+    ip = aip.join('.');
+    var tail = Array.apply(null, {
+        length: 255
+    }).map(Number.call, Number);
+    tail.forEach(function(t) {
+        var socket = io('http://' + ip + '.' + t + ':2389')
+        setTimeout(function() {
+            if (socket.connected)
+                serverFound(ip + '.' + t, socket)
+            else
+                socket.close()
+        }, 1500);
+    });
+}
+
+
+function serverFound(ip, socket) {
+    console.log('server found!! ', ip);
+    socket.emit('hello');
 }
