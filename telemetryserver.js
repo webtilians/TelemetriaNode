dc692f8303bbcc024fb3f813ff6699470913c838
enrikelr <enrikelr@hotmail.com>
2016-12-28 05:39:59 +0100

    rooms

--- telemetryserver.js
+++ telemetryserver.js
@@ -1,27 +1,37 @@
 var io = require('socket.io')(2389);
 console.log("hola telemetricos");
 var aSockets = {};
+var equipos={};
+var aPcConectados=[];
+
+   // aSockets[idUserto].emit('socketNewMsg', message);
 
 io.on('connection', function (socket) {
+    socket.on('pcConnected',function(idpc){
+        aSockets[data.idpc]= socket;
+        socket.join(idpc);
+    });
     
-    aSockets[data.idUser] = socket;
-    console.log(Object.keys(aSockets));
+    
     console.log('a user connected->', socket.acks.id);
     socket.emit("hola", {saludo: "hola"});
     socket.on('CarSetup', function (data) {
         console.log(data);
     });
+    console.log(Object.keys(aSockets));
     socket.on("connectUser", function (data) {
         console.log("conectUser")
         console.log(data.id)
-
+        aSockets[data.id]= socket;
+        socket.join(id);
     });
     socket.on('dashboard', function (data) {
-        console.log(data.Speed);
-        io.emit('dashboardClient', data);
+        io.sockets.in(room).emit('dashboardClient', data);
+        
     });
     socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
 });
+
