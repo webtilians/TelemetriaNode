84a67a03264f54688da0af44c34c2724cd96bed4
enrikelr <enrikelr@hotmail.com>
2016-12-30 06:07:37 +0100

    Update telemetriaClient.js

--- telemetriaClient.js
+++ telemetriaClient.js
@@ -3,10 +3,10 @@ var file = require('file-system');
 var fs = require('fs');
 var moment = require('moment')
 var datosTelemetria={}
-
+const idPc="pepitogrillo"
 // update telemetry as fast as possible
 var io =require('socket.io-client');
-irsdk.init({telemetryUpdateInterval: 3000})
+irsdk.init({telemetryUpdateInterval: 0})
 var iracing = irsdk.getInstance()
 var socket =io('http://vps259018.ovh.net:2389')
 
@@ -19,6 +19,7 @@ function saveSample (type, time, data) {
 
 iracing.on('Connected', function () {
   console.log('connected')
+  socket.emit('pcConnected',{idPc:idPc})
   iracing.once('TelemetryDescription', function (data) {
 	//console.log('TelemetryDesc:', data)
 	 var dateStr = moment().format().replace(/:/g, '')
@@ -29,15 +30,14 @@ iracing.on('Connected', function () {
 	for(var i in data){
 		datosTelemetria[i]=[]
 	}
-  }) 
+  })
 	iracing.on('SessionInfo', function (data) {
 		//console.log('Telemetry:',data.data.CarSetup)
-		socket.emit("CarSetup",data.data.CarSetup)
+		socket.emit("sessionData",data)
 	})
-  
+
 	iracing.on('Telemetry', function (data) {
-	console.log('Telemetry:', data.values.Speed)
-	socket.emit("speed",data.values.Speed)
+	socket.emit("dashboard",data.values)
 	for(var i in data.values){
 		datosTelemetria[i].push(data.values[i])
 		}
